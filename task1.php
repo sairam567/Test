92cd28b98069973dd4a1e6a0c898eba635a30bf0
sairam567 <sairamreddy9866@gmail.com>
2020-10-03 16:41:00 +0530

    modified task

--- task1.php
+++ task1.php
@@ -1,27 +1,12 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document Test</title>
-    ead>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Services</title>
-    require_once 'mailchimp/vendor/autoload.php';
-function vtMailChimpAddSubscriber($entityData){
-	$adb = PearDatabase::getInstance();
-	$wsId   	= $entityData->get('assigned_user_id');
-	$parts 		= explode('x', $wsId);
-	$entityId 	= $parts[1];
-	$recordId = $entityData->getId();
-    $recordId = explode('x', $recordId);;
-    $recordId = $recordId[1];
+    this is testing
 </head>
-<body>
-   <p>This is for testing</p> 
+<bo<h5> services example</h5>
     <p>
-    	this is for modification in the 
+    	this is for modification in the ,
     </p>
+	<p> This is the modification</p>
 </body>
 </html>
