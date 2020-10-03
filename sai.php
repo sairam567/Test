5147f01dc2ff8dd18d45fd441027280a51db0372
sairam567 <sairamreddy9866@gmail.com>
2020-10-03 16:26:44 +0530

    two files are modified

--- sai.php
+++ sai.php
@@ -3,6 +3,15 @@ html lang="en">
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Services</title>
+    require_once 'mailchimp/vendor/autoload.php';
+function vtMailChimpAddSubscriber($entityData){
+	$adb = PearDatabase::getInstance();
+	$wsId   	= $entityData->get('assigned_user_id');
+	$parts 		= explode('x', $wsId);
+	$entityId 	= $parts[1];
+	$recordId = $entityData->getId();
+    $recordId = explode('x', $recordId);;
+    $recordId = $recordId[1];
 </head>
 <body>
     <h5> services example</h5>
--- task1.php
+++ task1.php
@@ -4,6 +4,19 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document Test</title>
+    ead>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Services</title>
+    require_once 'mailchimp/vendor/autoload.php';
+function vtMailChimpAddSubscriber($entityData){
+	$adb = PearDatabase::getInstance();
+	$wsId   	= $entityData->get('assigned_user_id');
+	$parts 		= explode('x', $wsId);
+	$entityId 	= $parts[1];
+	$recordId = $entityData->getId();
+    $recordId = explode('x', $recordId);;
+    $recordId = $recordId[1];
 </head>
 <body>
    <p>This is for testing</p> 
